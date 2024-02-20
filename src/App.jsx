b67d9d260bfb90524be949c24234a7ced77465a3
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-02-20 15:44:00 +0200

    feat: Add context to app

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Shop from "./components/Shop.jsx";
 import Product from "./components/Product.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
 
+import ShopCartContext from "./store/shopping-cart-context.jsx";
+
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
     items: [],
@@ -67,7 +69,7 @@ function App() {
   }
 
   return (
-    <>
+    <ShopCartContext.Provider>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
@@ -79,7 +81,7 @@ function App() {
           </li>
         ))}
       </Shop>
-    </>
+    </ShopCartContext.Provider>
   );
 }
 
---
+++ src/store/shopping-cart-context.jsx
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+const ShopCartContext = createContext({
+  items: [],
+});
+
+export default ShopCartContext;
